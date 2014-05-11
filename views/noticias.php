fce781254ccf5742137331eef791b65a05ff9cc9
juan2ramos <juan2ramos@gmail.com>
2014-05-11 10:25:00 -0500

    + Plantilla completa

---
+++ views/noticias.php
@@ -0,0 +1,32 @@
+<section id="<?php echo($url) ?>">
+    <h2 class="<?php echo($url) ?>">circulart</h2>
+    <article>
+        <img src="images/img1.jpg" alt=""/>
+        <h3>Circulart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx</h3>
+        <p>Circulart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx </p>
+        <canvas id="myCanvas"></canvas>
+    </article>
+</section>
+
+
+<script>
+
+
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext('2d');
+        doResize();
+        window.onresize=function() { doResize(); };
+         function doResize()
+        {
+
+            context.canvas.width = window.innerWidth-20;
+            context.beginPath();
+            context.moveTo(0, 10);
+            context.lineTo(500, 10);
+            context.lineTo(1020, 80);
+            context.lineTo(window.innerWidth-20, 20);
+
+            context.stroke();
+        }
+
+</script>
---
+++ views/portafolio.php
@@ -0,0 +1,7 @@
+<section id="<?php echo($url) ?>">
+    <h2 class="<?php echo($url) ?>">circulart</h2>
+    <article>
+        <img src="images/img1.jpg" alt=""/>
+
+    </article>
+</section>
